1ba8d70806bdc0cad873c28cca929325c65eed64
shubhamdangare <shubham17495@gmail.com>
2018-07-07 18:59:20 +0530

    new file:   Basic5/Fourth/.editorconfig 	new file:   Basic5/Fourth/.gitignore 	new file:   Basic5/Fourth/README.md 	new file:   Basic5/Fourth/angular.json 	new file:   Basic5/Fourth/e2e/protractor.conf.js 	new file:   Basic5/Fourth/e2e/src/app.e2e-spec.ts 	new file:   Basic5/Fourth/e2e/src/app.po.ts 	new file:   Basic5/Fourth/e2e/tsconfig.e2e.json 	new file:   Basic5/Fourth/package-lock.json 	new file:   Basic5/Fourth/package.json 	new file:   Basic5/Fourth/src/app/app-routing.module.ts 	new file:   Basic5/Fourth/src/app/app.component.css 	copied:     Basic5/Third/src/app/app.component.html -> Basic5/Fourth/src/app/app.component.html 	new file:   Basic5/Fourth/src/app/app.component.spec.ts 	new file:   Basic5/Fourth/src/app/app.component.ts 	new file:   Basic5/Fourth/src/app/app.module.ts 	new file:   Basic5/Fourth/src/app/validate/validate.component.css 	new file:   Basic5/Fourth/src/app/validate/validate.component.html 	new file:   Basic5/Fourth/src/app/validate/validate.component.spec.ts 	new file:   Basic5/Fourth/src/app/validate/validate.component.ts 	new file:   Basic5/Fourth/src/assets/.gitkeep 	new file:   Basic5/Fourth/src/browserslist 	new file:   Basic5/Fourth/src/environments/environment.prod.ts 	new file:   Basic5/Fourth/src/environments/environment.ts 	new file:   Basic5/Fourth/src/favicon.ico 	new file:   Basic5/Fourth/src/index.html 	new file:   Basic5/Fourth/src/karma.conf.js 	new file:   Basic5/Fourth/src/main.ts 	new file:   Basic5/Fourth/src/polyfills.ts 	new file:   Basic5/Fourth/src/styles.css 	new file:   Basic5/Fourth/src/test.ts 	new file:   Basic5/Fourth/src/tsconfig.app.json 	new file:   Basic5/Fourth/src/tsconfig.spec.json 	new file:   Basic5/Fourth/src/tslint.json 	new file:   Basic5/Fourth/tsconfig.json 	new file:   Basic5/Fourth/tslint.json 	new file:   Basic5/Third/package-lock.json 	modified:   Basic5/Third/package.json 	modified:   Basic5/Third/src/app/app.component.html 	modified:   Basic5/Third/src/app/app.module.ts 	new file:   Basic5/Third/src/app/validate/validate.component.css 	new file:   Basic5/Third/src/app/validate/validate.component.html 	new file:   Basic5/Third/src/app/validate/validate.component.spec.ts 	new file:   Basic5/Third/src/app/validate/validate.component.ts

--- Basic5/Fourth/src/app/app.module.ts
+++ Basic5/Fourth/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValidateComponent } from './validate/validate.component';
-import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,8 +12,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Basic5/Third/src/app/validate/validate.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-validate',
+  templateUrl: './validate.component.html',
+  styleUrls: ['./validate.component.css']
+})
+export class ValidateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  public Validate(data)
+  {
+    console.log(data);
+  }
+
+}
