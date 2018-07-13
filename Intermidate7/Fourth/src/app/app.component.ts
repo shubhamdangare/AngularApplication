69121e8e5cdd249933fb2acf43c0066a88784a7b
shubhamdangare <shubham17495@gmail.com>
2018-07-13 22:16:41 +0530

    modified:   Intermidate7/Fourth/package-lock.json 	modified:   Intermidate7/Fourth/package.json 	modified:   Intermidate7/Fourth/src/app/app.component.html 	modified:   Intermidate7/Fourth/src/app/app.component.ts 	modified:   Intermidate7/Fourth/src/app/app.module.ts 	modified:   Intermidate7/Fourth/src/styles.css

--- Intermidate7/Fourth/src/app/app.component.ts
+++ Intermidate7/Fourth/src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
+export interface PeriodicElement {
+  name: string;
+  pages: number;
+  price: number;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'karuanchi',pages: 700, price: 550},
+  {name: 'cracking code ',pages: 700, price: 550},
+  {name: 'robert',pages: 700, price: 550}
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +17,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  displayedColumns: string[] = ['name', 'pages', 'price'];
+  dataSource = ELEMENT_DATA;
 }
--- Intermidate7/Fourth/src/app/app.module.ts
+++ Intermidate7/Fourth/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatTableModule} from '@angular/material/table';
+import { FormsModule } from "@angular/forms";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+  BrowserAnimationsModule,  BrowserModule,MatTableModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
